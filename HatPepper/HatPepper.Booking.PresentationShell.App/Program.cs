6089e5e1d343c98fe5b3628aa0c2f088eb31971a
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-04-27 11:15:43 +0900

    APIキーの引数チェックを追加
    `using` ステートメントに新しい名前空間を追加し、APIキーが指定されていない場合にエラーメッセージを表示してプログラムを終了する処理を実装しました。指定されたAPIキーは `GourmetService.ApiKey` に設定され、その後 `FindNearbyController` が初期化されます。

--- HatPepper/HatPepper.Booking.PresentationShell.App/Program.cs
+++ HatPepper/HatPepper.Booking.PresentationShell.App/Program.cs
@@ -1,9 +1,16 @@
-﻿
-using HatPepper.Booking.App.Controller;
+﻿using HatPepper.Booking.App.Controller;
 using HatPepper.Booking.App.UseCase;
 using HatPepper.Booking.App.View;
 using HatPepper.Booking.Gateway.Device;
 using HatPepper.Booking.Gateway.HotPepper;
+
+if(args.Length == 0)
+{
+    Console.WriteLine("APIキーを指定してください。");
+    return;
+}
+
+GourmetService.ApiKey = args[0];
 
 FindNearbyController controller = new FindNearbyController(
     new FindNearbyUseCase(
